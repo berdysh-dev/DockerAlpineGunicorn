688eff0707175bb61217ce6b6769e55bc5e3acb9
root <root@berdysh.net>
2023-06-24 18:43:46 +0900

    Test

--- settings.py
+++ settings.py
@@ -1,16 +1,15 @@
 wsgi_app = 'hogeapp.wsgi:application'
 chdir = '/usr/local/Gunicorn'
+bind = '0.0.0.0:8001'
 
 daemon = False
+
+workers = 9
 
 raw_env = [
     'ENV_TYPE=dev',
     'HOGEHOGE_KEY=xxxxxxxxxxxxxxxxxxxxxxxxx'
 ]
-
-bind = '0.0.0.0:8001'
-
-workers = 2
 
 accesslog = '-'
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
